b49a44f8828c109e35dcec5b11fb2436836a60a7
Antony Peacock <ant.peacock@gmail.com>
2020-01-15 22:30:52 +0000

    Is binary tree func and test

--- topics/trees/binary.hpp
+++ topics/trees/binary.hpp
@@ -6,6 +6,7 @@
 #include <cassert>
 #include <functional>
 #include <string_view>
+#include <numeric>
 
 struct binary_node
 {
@@ -16,6 +17,7 @@ struct binary_node
 
 binary_node* create_binary_search_tree(std::vector<int> input);
 void destroy_binary_tree(binary_node*& tree);
+bool is_binary_search_tree(binary_node* tree, int min = std::numeric_limits<int>::min(), int max = std::numeric_limits<int>::max());
 binary_node* sorted_array_to_bst(const std::vector<int>& input, int start, int end);
 std::vector<int> preorder_recursive(binary_node* node);
 std::vector<int> inorder_recursive(binary_node* node);
